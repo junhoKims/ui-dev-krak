6e9ed97e05d2a5de19cd4502d8efaee7ba19c00b
krak <kraksin12@gmail.com>
2025-11-15 00:38:52 +0900

    chore: package.json 및 biome 추가

---
+++ src/Button/Button.jsx
@@ -0,0 +1,11 @@
+export function Button() {
+  console.log('sleifnsf');
+  const krak = 'krak';
+
+  return (
+    <div>
+      <div>{`${krak} hello`}</div>
+      <button type="button" />
+    </div>
+  );
+}
